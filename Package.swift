a42571689afdcd11a332ad1bc012d42152bb575f
Halit S. Baskurt <baskurthalit@gmail.com>
2024-06-24 15:48:17 +0300

    Add macOS support.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "asnycImage",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12),.macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
